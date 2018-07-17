656d90904da78fce6472a52588a747db101bafb0
Roman Kryvoshapka <kryvoshapka.roman@gmail.com>
2018-07-17 23:16:07 +0300

    pagination

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kryvoshapka.roman@gmail.com']
+    POSTS_PER_PAGE = 3
+
 
 
 
